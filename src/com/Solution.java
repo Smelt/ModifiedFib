a38a7849f0b11eb3bad32357c31448995aba2802
Smelt <shivamsatyarthi@gmail.com>
2017-02-19 22:59:37 -0500

    inital commit

---
+++ src/com/Solution.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Solution {
+
+
+	public static void main(String[] args) {
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+	}
+}
